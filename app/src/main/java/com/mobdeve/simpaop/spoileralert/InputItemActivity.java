ba80bbacc471af3d073e36bfaba6c1cade0e26e7
PtrckSimps <patrick_lawrence_simpao@dlsu.edu.ph>
2021-05-11 14:29:00 +0800

    Added layout for InputItemActivity

---
+++ app/src/main/java/com/mobdeve/simpaop/spoileralert/InputItemActivity.java
@@ -0,0 +1,56 @@
+package com.mobdeve.simpaop.spoileralert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class InputItemActivity extends AppCompatActivity {
+
+    private static final String TAG = "InputItemActivity";
+
+    private TextView expiryDateInput;
+    private DatePickerDialog.OnDateSetListener expiryDateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_item);
+
+        this.expiryDateInput = findViewById(R.id.expiryDateInput);
+
+        expiryDateInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(InputItemActivity.this, android.R.style.Theme_Holo_Dialog_NoActionBar_MinWidth, expiryDateListener, year, month, day);
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        expiryDateListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+
+                String date = month + "/" + dayOfMonth + "/" + year;
+                expiryDateInput.setText(date);
+            }
+        };
+    }
+}
--- app/src/main/java/com/mobdeve/simpaop/spoileralert/MainActivity.java
+++ app/src/main/java/com/mobdeve/simpaop/spoileralert/MainActivity.java
@@ -2,13 +2,31 @@ package com.mobdeve.simpaop.spoileralert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button addItemBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.addItemBtn = findViewById(R.id.addItemBtn);
+
+        this.addItemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, InputItemActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
