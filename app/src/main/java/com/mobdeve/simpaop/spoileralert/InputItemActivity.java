37641c9e5841dfde5279bbe1e48a79a24c3c8668
hans-ramos <jester_ramos@dlsu.edu.ph>
2021-05-16 20:40:15 +0800

    Added camera input

--- app/src/main/java/com/mobdeve/simpaop/spoileralert/InputItemActivity.java
+++ app/src/main/java/com/mobdeve/simpaop/spoileralert/InputItemActivity.java
@@ -1,16 +1,26 @@
 package com.mobdeve.simpaop.spoileralert;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,6 +38,8 @@ public class InputItemActivity extends AppCompatActivity {
     private EditText itemCategoryEt;
     private EditText quantityEt;
     private Calendar calendar = Calendar.getInstance();
+    private Button addPictureBtn, addExpiryBtn;
+    private ImageView itemPictureIv, itemExpiryIv;
     private DatePickerDialog.OnDateSetListener expiryDateListener;
     private ItemAdapter adapter;
     DatabaseHelper databaseHelper;
@@ -43,6 +55,10 @@ public class InputItemActivity extends AppCompatActivity {
         this.itemNameEt = findViewById(R.id.itemNameEt);
         this.itemCategoryEt = findViewById(R.id.itemCategoryEt);
         this.quantityEt = findViewById(R.id.quantityEt);
+        this.addPictureBtn = findViewById(R.id.addPictureBtn);
+        this.itemPictureIv = findViewById(R.id.itemPictureIv);
+        this.itemExpiryIv = findViewById(R.id.itemExpiryIv);
+        this.addExpiryBtn = findViewById(R.id.addExpiryProofBtn);
 
 
         this.expiryDateInput = findViewById(R.id.expiryDateInput);
@@ -67,6 +83,28 @@ public class InputItemActivity extends AppCompatActivity {
                         calendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+
+        if(ContextCompat.checkSelfPermission(InputItemActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(InputItemActivity.this, new String[] {
+                    Manifest.permission.CAMERA
+            }, 100);
+        }
+        addPictureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+
+        addExpiryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 200);
+            }
+        });
+
     }
 
     private void updateLabel() {
@@ -74,6 +112,19 @@ public class InputItemActivity extends AppCompatActivity {
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat);
 
         expiryDateInput.setText(sdf.format(calendar.getTime()));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            itemPictureIv.setImageBitmap(Bitmap.createScaledBitmap(captureImage, 240, 240, false));
+        }
+        else if (requestCode == 200) {
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            itemExpiryIv.setImageBitmap(Bitmap.createScaledBitmap(captureImage, 240, 240, false));
+        }
     }
 
     public void addItem(View view){
